4bfd735f1503c625fd1be7d5d3a025ef04618622
chandan0134 <chandanma39@gmail.com>
2023-09-27 02:06:48 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM,{ createRoot } from "react-dom/client"
 import {Header} from "./components/header.js"
 import Body from "./components/Body"
-import Footer from "./components/Body"
+import Footer from "./components/Footer"
 
 
 const AppLayout =()=>{
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,59 @@
 
 import {restaurantList} from "../constants"
 import ResCard from "./ResCard"
-import {useState} from react;
+import {useState} from "react";
+
+function filterSearch(searchQuery,restaurant){
+    const filterSearch=restaurant.filter((restaurant)=>
+      restaurant.data.name.includes(searchQuery)
+    );
+    return filterSearch;
+}
+
 const Body =()=>{
 
     const [searchQuery, setSearchQuery] = useState('');
-    
+    const [restaurants, setRestaurants]=useState(restaurantList);
     return(
+    <>
+    <div className="search-container">
+        <input
+        type="text"
+        className="search-"
+        placeholder="search"
+        value={searchQuery}
+        onChange={(e)=>{
+            setSearchQuery(e.target.value);
+        }}
+        />
+        <button
+        className="search-btn"
+        onClick={()=>{
+            const data=filterSearch(searchQuery,restaurants);
+            setRestaurants(data);
+        }
+        }
+        >
+        search
+        </button>
+    </div>
+
     <div className="res-list">
-        {restaurantList.map((restaurant)=>{
-            return <ResCard key={restaurant.data.id}  {...restaurant.data} />
+    
+        { restaurants.map((restaurant) => {
+            return (
+            <ResCard key={restaurant.data.id}  {...restaurant.data} />
+            );
         })}
+
+     
+
+
         
     </div>
+    </>
     );
+  
 
 };
      

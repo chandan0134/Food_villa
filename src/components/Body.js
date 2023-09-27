0f0f16ff804b782d58c739d3d1a3b90172d82a27
chandan0134 <chandanma39@gmail.com>
2023-09-28 02:48:33 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,60 +1,46 @@
+import { restaurantList } from "../constants";
+import ResCard from "./ResCard";
+import { useState } from "react";
 
-import {restaurantList} from "../constants"
-import ResCard from "./ResCard"
-import {useState} from "react";
-
-function filterSearch(searchQuery,restaurant){
-    const filterSearch=restaurant.filter((restaurant)=>
-      restaurant.data.name.includes(searchQuery)
-    );
-    return filterSearch;
+function filterData(searchQuery, restaurants) {
+  return restaurants.filter((restaurant) =>
+    restaurant.data.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 }
 
-const Body =()=>{
+const Body = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurantList);
 
-    const [searchQuery, setSearchQuery] = useState('');
-    const [restaurants, setRestaurants]=useState(restaurantList);
-    return(
+  const handleSearch = () => {
+    const filteredData = filterData(searchQuery, restaurantList);
+    setFilteredRestaurants(filteredData);
+  };
+
+  return (
     <>
-    <div className="search-container">
+      <div className="search-container">
         <input
-        type="text"
-        className="search-"
-        placeholder="search"
-        value={searchQuery}
-        onChange={(e)=>{
+          type="text"
+          className="search-"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => {
             setSearchQuery(e.target.value);
-        }}
+          }}
         />
-        <button
-        className="search-btn"
-        onClick={()=>{
-            const data=filterSearch(searchQuery,restaurants);
-            setRestaurants(data);
-        }
-        }
-        >
-        search
+        <button className="search-btn" onClick={handleSearch}>
+          Search
         </button>
-    </div>
+      </div>
 
-    <div className="res-list">
-    
-        { restaurants.map((restaurant) => {
-            return (
-            <ResCard key={restaurant.data.id}  {...restaurant.data} />
-            );
-        })}
-
-     
-
-
-        
-    </div>
+      <div className="res-list">
+        {filteredRestaurants.map((restaurant) => (
+          <ResCard key={restaurant.data.id} {...restaurant.data} />
+        ))}
+      </div>
     </>
-    );
-  
-
+  );
 };
-     
+
 export default Body;
